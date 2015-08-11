b159ec69c8e21c9ae7fbc9f5a78db29b2451071f
zzzhan <imailer@sina.com>
2015-08-11 21:53:15 +0800

    del useless file

--- cipher/src/js/shapefly-message.js
+++
@@ -1,3 +0,0 @@
-_1×ƒ:PéKÙžÀé§ú ;cVö=QPÛ×2]Ö]wnE1M+‹© êÞ8BžzpÎJïúæt=MW¥mgÖ‘³eR¹W§ù’Tb@Ó
-]’ëFæûDúø…qÈö¨éˆÂ¨ós†zÏ-!³+\Œ˜zöÔÉ1Ö0PVÞ’'¯TOrÓoÙ‹&{Ä;7kÎX,ªìGÂa8£rÿb¹
-ÅfaÓði,Ö.9û`œ ¬¬Ÿš=ËJIˆæ©$äbQ£¬›	®$éeG²ÞÑ_IrÖ§É2¡Kò³à×Ÿ¹‘±IJ^˜–ï€ÁößƒœE¡e”>¿¨`‚¡±£ÕgÈµ~ÊÀ4[,¤ßëÕzØ›£›í+2ìÃ[†}›ê » güaÒ½lJåø4¾Ì]àµÍ•Ä­ª¤ØBé
